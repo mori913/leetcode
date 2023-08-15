9af272455e0063fde9e9f91bf73ea3e9e4b5106e
you mosen <yemaosen@g.ecc.u-tokyo.ac.jp>
2023-08-15 21:36:04 +0900

    test

---
+++ 205.isomorphic-strings.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=205 lang=cpp
+ *
+ * [205] Isomorphic Strings
+ */
+#include "stdc++.h"
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+       unordered_map<char, char> map;
+       int n = s.length();
+       return true; 
+    }
+};
+// @lc code=end
+
